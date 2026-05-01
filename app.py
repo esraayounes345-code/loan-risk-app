e71364e242b3776be6b08a7773153e06a1116291
esraayounes345-code <esraayounes345@gmail.com>
2026-05-01 23:53:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
 import streamlit as st
 import pickle
-import pandas as pd
 
-# تحميل الموديل
 model = pickle.load(open("loan_model.pkl", "rb"))
 
-# عنوان التطبيق
 st.title("Loan Risk App")
 
-# إدخال البيانات
 age = st.number_input("Age")
 income = st.number_input("Income")
 
-# زر التنبؤ
-if st.button("Predict"):
-    data = pd.DataFrame([[age, income]])
-    prediction = model.predict(data)
+st.write(model.feature_names_in_)
 
-    st.write("Prediction:", prediction[0])
+if st.button("Predict"):
+    st.write("App works successfully")
