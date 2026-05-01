94529f81220fa5190a81a2608fc96085feea514f
esraayounes345-code <esraayounes345@gmail.com>
2026-05-01 22:22:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 
+import streamlit as st
+
+st.title("Loan Risk App")
+
+age = st.number_input("Age")
+income = st.number_input("Income")
+
+if st.button("Predict"):
+    st.write("App works successfully")
