fab4a5e2d935fe0063b432668f54d95da27dee3f
esraayounes345-code <esraayounes345@gmail.com>
2026-05-01 23:56:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 import streamlit as st
 import pickle
+import pandas as pd
 
 model = pickle.load(open("loan_model.pkl", "rb"))
 
 st.title("Loan Risk App")
 
 age = st.number_input("Age")
-income = st.number_input("Income")
-
-st.write(model.feature_names_in_)
+income = st.number_input("Annual Income")
+loan_amount = st.number_input("Loan Amount")
+interest_rate = st.number_input("Loan Interest Rate")
+employment_years = st.number_input("Employment Years")
+credit_score = st.number_input("Credit Score")
 
 if st.button("Predict"):
-    st.write("App works successfully")
+
+    data = pd.DataFrame({
+        "Age": [age],
+        "Annual_Income": [income],
+        "Loan_Amount": [loan_amount],
+        "Loan_Interest_Rate": [interest_rate],
+        "Employment_Years": [employment_years],
+        "Credit_Score": [credit_score]
+    })
+
+    prediction = model.predict(data)
+
+    st.write("Prediction:", prediction[0])
